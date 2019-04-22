a7b81dfabe1e42cffc545b24d3fe2d6b9bf07c25
Robert Gauld <robert@robertgauld.co.uk>
2019-04-22 20:10:57 +0100

    Bump version for more development

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Version 0.0.3
+
+  * ...
+
 ## Version 0.0.2
 
   * Add support for:
--- lib/rail_feeds/version.rb
+++ lib/rail_feeds/version.rb
@@ -5,7 +5,7 @@ module RailFeeds
   class Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 2
+    PATCH = 3
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
